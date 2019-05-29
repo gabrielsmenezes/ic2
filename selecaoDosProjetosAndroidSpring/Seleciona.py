73328c602cfc2e7ff5e09d81c85e9b6dfa8d7362
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-05-29 10:24:15 -0400

    Esqueleto da selecao dos projetos

---
+++ selecaoDosProjetosAndroidSpring/Seleciona.py
@@ -0,0 +1,7 @@
+#Ler do arquivo os enderecos
+    #para cada endereco:
+        #baixar o repositorio
+        #Verificar se eh spring ou android
+            #se for android printa ('android', nomeDoRepositorio)
+            #se for spring printa ('spring', nomeDoRepositorio)
+            #apagar projeto
