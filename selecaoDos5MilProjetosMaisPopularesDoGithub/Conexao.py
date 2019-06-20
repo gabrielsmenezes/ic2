65f6c82e1c6a82ff75ce39faa7333e17eb9224c5
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-06-20 13:05:04 -0400

    Conseguindo selecionar os 5 mil projetos java

--- selecaoDos5MilProjetosMaisPopularesDoGithub/Conexao.py
+++ selecaoDos5MilProjetosMaisPopularesDoGithub/Conexao.py
@@ -2,43 +2,51 @@ import http.client
 import json
 import time
 
-
-class Conexao:
-
-	def __init__(self):
-		pass
-
-	def criaConexaoComServidor(self):
-		return http.client.HTTPSConnection('api.github.com', 443)
+def criaConexaoComServidor():
+	return http.client.HTTPSConnection('api.github.com', 443)
 
 
-	def criaCabecalho(self):
-		cabecalho = {
-			'Content-type': 'application/json',
-			'User-Agent': 'gabrielsmenezes',
-			'Authorization': 'Bearer a98cb10f229115299a35d7e21ee137ba2bf8ec14'
-		}
-		return cabecalho
-	
-	def getRepositories(self):
-		
-		resposta = dict()
-		for i in range(1, 35):
-			conexao = self.criaConexaoComServidor()
-			cabecalho = self.criaCabecalho()
-			corpoDaRequisicaoEmJSON = ""		
-			time.sleep(60)
-			print(i)
-			conexao.request('GET', "/search/repositories?q=language:java&sort=stars&order=desc&page="+str(i), corpoDaRequisicaoEmJSON, cabecalho)
-			response = conexao.getresponse()
-			respostaEmJson = json.loads(response.read().decode())
+def criaCabecalho():
+	cabecalho = {
+		'Content-type': 'application/json',
+		'User-Agent': 'gabrielsmenezes',
+		'Authorization': 'Bearer 82168313e7fbd3d04983e796555596b4b955d08b'
+	}
+	return cabecalho
+
+def getRepositories():
+	numero_de_estrelas = 80000
+	resposta = dict()
+	conexao = criaConexaoComServidor()
+	cabecalho = criaCabecalho()
+	corpoDaRequisicaoEmJSON = ""
+	with open ('saida.csv', 'w') as arquivo:
+		while(len(resposta) < 5000):
 			try:
-				repositorios = respostaEmJson['items']
-				for i in range(0, 30):
-					full_name = repositorios[i]['full_name']
-					stargazers_count = repositorios[i]['stargazers_count']
-					resposta[full_name] = stargazers_count
-			except:
+				conexao.request('GET', "/search/repositories?q=language:java"+"+stars:<="+str(numero_de_estrelas)+"&sort=stars&order=desc", corpoDaRequisicaoEmJSON, cabecalho)
+				response = conexao.getresponse()
+				rate_limit_remaing = int(response.headers["X-RateLimit-Remaining"][0])
+				if (rate_limit_remaing < 5):
+					print("Spleeping")
+					time.sleep(10)
+					print("waking up")
+				respostaEmJson = json.loads(response.read().decode())
+				repositorios = respostaEmJson["items"]
+				for repositorio in repositorios:
+					full_name = repositorio['full_name']
+					stargazers_count = repositorio['stargazers_count']
+					resposta[full_name] = stargazers_count	
+					numero_de_estrelas = stargazers_count
+					print(full_name, numero_de_estrelas)
+					arquivo.write("%s,%s\n" % (full_name, numero_de_estrelas))
+					print("Projetos ja encontrados: " + str(len(resposta)))
+			except Exception as identifier:
+				print(identifier.__str__())
+				print("============================\n")
 				print(respostaEmJson)
+	return resposta
 
-		return resposta
+
+
+if __name__ == "__main__":
+	getRepositories()
