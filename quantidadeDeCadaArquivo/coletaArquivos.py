7989b93157fe5ede5dd2e28f8bed3255c5bd9c3d
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-06-26 19:33:36 -0400

    Fazendo o tratamento para desconsiderar arquivos dentor do .git

--- quantidadeDeCadaArquivo/coletaArquivos.py
+++ quantidadeDeCadaArquivo/coletaArquivos.py
@@ -3,6 +3,8 @@ import os, fnmatch
 def find(pattern, path):
 	result = []
 	for root, dirs, files in os.walk(path):
+		if('.git' in root):
+			continue
 		for name in files:
 			if fnmatch.fnmatch(name, pattern):
 				result.append(os.path.join(root, name))
