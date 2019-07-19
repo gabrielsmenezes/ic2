d19dbc9ea13a0c4f00eb871241b3a4fcaff72cf8
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-07-19 09:17:15 -0400

    Gerando os graficos

--- analiseDosProjetosGerais/RQ4/Graficos/RQ4.R
+++ analiseDosProjetosGerais/RQ4/Graficos/RQ4.R
@@ -4,6 +4,9 @@ library(devtools)
 library(easyGgplot2)
 library(forcats)
 
+
+##############Number of forks
+
 all=read.csv("/home/gabriel/Documentos/ic2/analiseDosProjetosGerais/RQ1/obtendoOsMetadadosDosRepositorios/listaDeMetadadosDosProjetos.csv", sep=",",header=T)
 
 p1 <- ggplot(all, aes(factor(all$framework, levels = c("Android","Spring")), all$forks)) + 
@@ -20,6 +23,9 @@ p1
 
 ggsave("/home/gabriel/Documentos/ic2/analiseDosProjetosGerais/RQ4/Graficos/forks.pdf", width = 4.5, height = 4.5)
 
+
+
+##########number of imports
 all=read.csv("/home/gabriel/Documentos/ic2/analiseDosProjetosGerais/RQ4/numeroDeImports/imports.csv", sep=",",header=T)
 
 
@@ -35,6 +41,8 @@ p1
 
 ggsave("/home/gabriel/Documentos/ic2/analiseDosProjetosGerais/RQ4/graficos/totalImportsDistinct.pdf", width = 4.5, height = 4.5)
 
+
+
 ####Mantenedores
 
 all = read.csv("/home/gabriel/Documentos/ic2/analiseDosProjetosGerais/RQ4/mantenedores/juntos.csv",sep=",",header=T)
@@ -49,3 +57,37 @@ p1 <- ggplot(all, aes(factor(all$framework,levels = c("Android","Spring")), (all
 p1
 
 ggsave("/home/gabriel/Documentos/ic2/analiseDosProjetosGerais/RQ4/Graficos/FrameworkInCodeSample.pdf", width = 4.5, height = 4.5)
+
+
+##############forks ahead / forks
+
+all=read.csv("/home/gabriel/Documentos/ic2/analiseDosProjetosGerais/RQ4/extraindoTotalDeForks_ForksAhead/forks.csv", sep=",",header=T)
+
+p1 <- ggplot(all, aes(factor(all$framework,levels = c("Android","Spring")), (all$forks_ahead/all$forks)*100)) + 
+  geom_violin(width=1, trim=TRUE, fill="#87CEFA") + 
+  #scale_y_log10() +
+  geom_boxplot(width=0.7,alpha=0.7) + ggtitle("Relative Ahead Forks") + xlab("General Projects") + ylab("Percent of Ahead Forks") + 
+  annotate("text", x = 1, y = 3, label = "2", size = 6) + 
+  annotate("text", x = 2, y = 4, label = "3", size = 6) +
+  theme(plot.title=element_text(size=20,face="bold") ,axis.title=element_text(size=18),axis.text=element_text(size=18))
+
+p1
+
+ggsave("/home/gabriel/Documentos/ic2/analiseDosProjetosGerais/RQ4/Graficos/razao.pdf", width = 4.5, height = 4.5)
+
+
+
+########### Relative projetos ahead by number of commits
+subtipos = rep(c("Android", "Spring"), each=4)
+apiLevel <- rep( c("1", "2-3", "4-10", "> 10"), 2 )
+values <-c(41,25,16,16,47,29,13,8)
+df2 <- data.frame(supp=subtipos,dose=apiLevel,len=values)
+head(df2)
+p <- ggplot(data=df2, aes(x=dose, y=len, fill=supp)) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  labs(title="Relative projects ahead by commits", x="Number of commits", y = "Percent of projects") +
+  scale_fill_manual("", values = c("Android" = "#87CEFA", "Spring" = "#4682b4"))+
+  geom_text(aes(label=len), vjust=0, color="black",position = position_dodge(0.9), size=5)+
+  theme(plot.title=element_text(size=16, face = "bold"), axis.title=element_text(size=16),axis.text=element_text(size=16), legend.position = c(0.87, 0.80))
+p
+ggsave("/home/gabriel/Documentos/ic2/analiseDosProjetosGerais/RQ4/Graficos/relative_projects_by_commits2.pdf", width = 4.5, height = 4.5) 
