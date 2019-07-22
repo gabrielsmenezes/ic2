e04a69d6626f3eaec61105fa305785be18d5eee9
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-07-22 16:01:50 -0400

    Arrumando os graficos

--- analiseDosProjetosGerais/RQ1/graficos/metricasBasicas.R
+++ analiseDosProjetosGerais/RQ1/graficos/metricasBasicas.R
@@ -9,7 +9,7 @@ p1 <- ggplot(all, aes(factor(all$framework,levels = c("Android", "Spring")), all
   scale_y_log10() +
   geom_violin(width=1, trim=TRUE, fill="#87CEFA") + 
   geom_boxplot(width=0.7,alpha=0.7) + ggtitle("Number of Stars") + xlab("General Projects") + ylab("Number of Stars (log scale)") + 
-  annotate("text", x = 1, y = 500, label = "388.5", size = 8) + annotate("text", x = 2, y = 450, label = "346", size = 8) +
+  annotate("text", x = 1, y = 500, label = "388", size = 8) + annotate("text", x = 2, y = 450, label = "346", size = 8) +
   theme(plot.title=element_text(size=24,face="bold") ,axis.title=element_text(size=20),axis.text=element_text(size=18))
 
 p1
@@ -39,7 +39,7 @@ p1 <- ggplot(all, aes(factor(all$framework,levels = c("Android", "Spring")), all
   scale_y_log10() +
   geom_violin(width=1, trim=TRUE, fill="#87CEFA") + 
   geom_boxplot(width=0.7,alpha=0.7) + ggtitle("Number of Files") + xlab("General Projects") + ylab("Number of Files (log scale)") + 
-  annotate("text", x = 1, y = 100, label = "77", size = 8) + annotate("text", x = 2, y = 300, label = "177.5", size = 8) +
+  annotate("text", x = 1, y = 100, label = "77", size = 8) + annotate("text", x = 2, y = 300, label = "177", size = 8) +
   theme(plot.title=element_text(size=24,face="bold") ,axis.title=element_text(size=20),axis.text=element_text(size=18))
 
 p1
